62610d1e5a534de2a80e3c6460ad97b04456efbd
cdemirel <candasd@gmail.com>
2020-10-08 21:24:44 +0300

    Initial Commit

---
+++ Assets/Scripts/ExampleSceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExampleSceneLoader : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            LoadScene();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            UnLoadScene();
+        }
+    }
+    private void LoadScene()
+    {
+        SceneManager.LoadScene((int)Scenes.UIScene, LoadSceneMode.Additive);
+    }
+
+    private void UnLoadScene()
+    {
+        SceneManager.UnloadSceneAsync((int)Scenes.UIScene);
+    }
+}
+
+public enum Scenes
+{
+    SampleScene,
+    UIScene
+}
